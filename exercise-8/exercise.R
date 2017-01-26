56ab2b90ba0680eac385a5107a14b413b92c9364
Kyle Simpson <kes97@uw.edu>
2017-01-25 17:43:01 -0800

    Complete exercise

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -29,13 +29,22 @@ SwissData <- mutate(SwissData, Mortality.Difference = Infant.Mortality - mean(In
 
 # Create a new data frame that only is that of regions that have a Infant mortality rate less than the
 # mean.  Have this data frame only have the regions name, education and mortality rate.
-
+reg.edu.mort <- filter(SwissData, Mortality.Difference < 0) %>% select(Region, Education, Infant.Mortality)
+View(reg.edu.mort)
 
 #Filter one of the columns based on a question that you may have (which regions have a higher
 #education rate, etc.) and write that to a csv file
-
+filter(reg.edu.mort, Infant.Mortality == max(Infant.Mortality)) %>% write.csv("Highest Infant Mortality")
 
 # Create a function that can take in two different region names and compare them based on a statistic
 # Of your choice (education, Examination, ect.)  print out a statment describing which one is greater
 # and return a data frame that holds the selected region and the compared variable.  If your feeling adventurous
 # also have your function write to a csv file.
+CompareRegionsEducation <- function(region1, region2) {
+  region <- filter(data, Region %in% c(region1, region2)) %>%
+    filter(Education %>% max(Education)) %>%
+    select(Region, Education)
+  file.path <- paste("data/", region1, "-versus-", region2, ".csv", sep="")
+  write.csv(region, file.path)
+  return (region)
+}
