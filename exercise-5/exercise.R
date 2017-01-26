b0ecda0e44b51424694c2220b53b5862a6375f39
Kyle Simpson <kes97@uw.edu>
2017-01-26 14:48:29 -0800

    Complete exercise

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -2,10 +2,14 @@
 
 # Install the nycflights13 package and read it in. Require the dplyr package.
 # install.packages("nycflights13")
-
+library(nycflights13)
+library(dplyr)
+View(flights)
 
 # In which month was the average departure delay the greatest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
+new.flights <- group_by(flights, month) %>% summarize(mean = mean(dep_delay, na.rm = TRUE)) %>% filter(mean == max(mean))
+View(new.flights)
 
 
 # If you create a data.frame with the columns "month", and "delay" above, you should be able to create
